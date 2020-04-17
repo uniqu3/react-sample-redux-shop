f7486e97cf9b578b0cf755dfaf70f1e11fa9059f
Goran Ilic (uniqu3) <uniqu3e@gmail.com>
2020-04-17 15:53:35 +0200

    heroku build

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -6,14 +6,7 @@ import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
     <HomePageContainer>
-        <Profiler
-            id="Directory"
-            onRender={(id, phase, actualDuration) => {
-                console.log({ id, phase, actualDuration });
-            }}
-        >
-            <Directory />
-        </Profiler>
+        <Directory />
     </HomePageContainer>
 );
 
