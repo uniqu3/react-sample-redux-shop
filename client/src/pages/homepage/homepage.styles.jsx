f474208f0daea1992daad1f969095ea25035fa96
Goran Ilic (uniqu3) <uniqu3e@gmail.com>
2020-04-17 11:08:02 +0200

    implementing lazy loaded compnents and error boundary

--- client/src/pages/homepage/homepage.styles.jsx
+++ client/src/pages/homepage/homepage.styles.jsx
@@ -4,5 +4,9 @@ export const HomePageContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 20px 80px;
+    padding: 20px 10px;
+
+    @media screen and (min-width: 800px) {
+        padding: 20px 80px;
+    }
 `;
