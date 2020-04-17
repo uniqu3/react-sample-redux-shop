813f58b906b7e52e9b4944ef338de0fa86b484d2
Goran Ilic (uniqu3) <uniqu3e@gmail.com>
2020-04-17 15:25:53 +0200

    updating code to pwa, added serviceworker and enforcing ssl

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
@@ -18,3 +19,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+serviceWorker.register();
